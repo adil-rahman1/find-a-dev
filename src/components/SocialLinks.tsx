d98c46214293ddf5035ae1f45f3136ad014672f6
Adil Rahman <82183381+adil-rahman1@users.noreply.github.com>
2025-02-07 00:20:11 +0000

    Refactored to allow editing the 'About me' section of profile

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,41 +1,34 @@
 import "../components/social-links.css";
-import ISocialLinks from "../types/SocialLinks";
 import LinkedInLogo from "../../public/assets/icons/linkedin.svg";
 import GitHubLogo from "../../public/assets/icons/github.svg";
 import WebsiteLogo from "../../public/assets/icons/website.svg";
 import OtherSiteLogo from "../../public/assets/icons/otherSite.svg";
-import { Dispatch, SetStateAction, useState } from "react";
+import ISocialLinks from "../types/SocialLinks";
 
 interface SocialLinksProps {
   isEditing: boolean;
-  setIsEditing: Dispatch<SetStateAction<boolean>>;
-  socialLinks: ISocialLinks;
+  savedSocialLinks: ISocialLinks;
+  editedSocialLinks: ISocialLinks;
+  setEditedSocialLinks: React.Dispatch<React.SetStateAction<ISocialLinks>>;
 }
 
 const SocialLinks = ({
   isEditing,
-  setIsEditing,
-  socialLinks,
+  savedSocialLinks,
+  editedSocialLinks,
+  setEditedSocialLinks,
 }: SocialLinksProps) => {
-  const [savedSocialLinks, setSavedSocialLinks] = useState(socialLinks);
-  const [editedSocialLinks, seEditedSocialLinks] = useState(socialLinks);
-
-  function saveNewSocialLinks() {
-    setSavedSocialLinks(editedSocialLinks);
-    setIsEditing(false);
-  }
-
   return (
     <div className="flex flex-col items-start">
       {savedSocialLinks.linkedin && (
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-2 mt-3">
           <img className="social-icon" src={LinkedInLogo} alt="Your SVG" />
           {isEditing && (
             <input
               className="border-2 border-black w-96 mb-2 p-0.5 rounded-md"
               value={editedSocialLinks.linkedin}
               onChange={(e) =>
-                seEditedSocialLinks({
+                setEditedSocialLinks({
                   ...editedSocialLinks,
                   linkedin: e.target.value,
                 })
@@ -53,7 +46,7 @@ const SocialLinks = ({
               className="border-2 border-black w-96 mb-2 p-0.5 rounded-md"
               value={editedSocialLinks.github}
               onChange={(e) =>
-                seEditedSocialLinks({
+                setEditedSocialLinks({
                   ...editedSocialLinks,
                   github: e.target.value,
                 })
@@ -71,7 +64,7 @@ const SocialLinks = ({
               className="border-2 border-black w-96 mb-2 p-0.5 rounded-md"
               value={editedSocialLinks.website}
               onChange={(e) =>
-                seEditedSocialLinks({
+                setEditedSocialLinks({
                   ...editedSocialLinks,
                   website: e.target.value,
                 })
@@ -89,7 +82,7 @@ const SocialLinks = ({
               className="border-2 border-black w-96 mb-2 p-0.5 rounded-md"
               value={editedSocialLinks.otherSite}
               onChange={(e) =>
-                seEditedSocialLinks({
+                setEditedSocialLinks({
                   ...editedSocialLinks,
                   otherSite: e.target.value,
                 })
@@ -98,30 +91,6 @@ const SocialLinks = ({
           )}
           {!isEditing && <span>{savedSocialLinks.otherSite}</span>}
         </div>
-      )}
-      {isEditing && (
-        <div>
-          <button
-            className=" text-white bg-green-400 hover:bg-green-500 p-1 w-12 ml-7 mr-2 rounded-md"
-            onClick={saveNewSocialLinks}
-          >
-            Save
-          </button>
-          <button
-            className="text-white bg-gray-500 hover:bg-gray-600 p-1 w-20 rounded-md"
-            onClick={() => setIsEditing(false)}
-          >
-            Cancel
-          </button>
-        </div>
-      )}
-      {!isEditing && (
-        <button
-          className="text-white bg-sky-600 hover:bg-sky-700 p-1 w-48 mt-2 rounded-md"
-          onClick={() => setIsEditing(true)}
-        >
-          Edit profile
-        </button>
       )}
     </div>
   );
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Pill from "../components/Pill";
 import PortfolioCard from "../components/PortfolioCard";
 import SocialLinks from "../components/SocialLinks";
+import ISocialLinks from "../types/SocialLinks";
 
 const socialLinks = {
   id: 1,
@@ -13,9 +14,21 @@ const socialLinks = {
 };
 
 const Profile = () => {
-  const [isEditing, setIsEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [savedAboutMe, setSavedAboutMe] = useState<string>("My name is Adil");
+  const [editedAboutMe, setEditedAboutMe] = useState<string>("My Name is Adil");
+  const [savedSocialLinks, setSavedSocialLinks] =
+    useState<ISocialLinks>(socialLinks);
+  const [editedSocialLinks, setEditedSocialLinks] =
+    useState<ISocialLinks>(socialLinks);
 
   const services = ["Web development", "Mobile development"];
+
+  function updateProfile() {
+    setSavedAboutMe(editedAboutMe);
+    setSavedSocialLinks(editedSocialLinks);
+    setIsEditing(false);
+  }
 
   return (
     <div className="p-4">
@@ -25,20 +38,48 @@ const Profile = () => {
         alt=""
       />
       <p className="font-bold text-xl my-4">John Smith</p>
+      <div>
+        {isEditing && (
+          <textarea
+            className="border-2 border-black w-96 mb-2 p-0.5 rounded-md"
+            value={editedAboutMe}
+            onChange={(e) => setEditedAboutMe(e.target.value)}
+          />
+        )}
+        {!isEditing && <p>{savedAboutMe}</p>}
+      </div>
       <SocialLinks
         isEditing={isEditing}
-        setIsEditing={setIsEditing}
-        socialLinks={socialLinks}
+        savedSocialLinks={savedSocialLinks}
+        editedSocialLinks={editedSocialLinks}
+        setEditedSocialLinks={setEditedSocialLinks}
       />
-      <p className="font-bold my-4">About me</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-        voluptas fugiat ducimus nihil hic vel. Commodi enim, debitis quam
-        blanditiis rem, iusto, delectus totam atque doloribus mollitia deserunt
-        ut laborum eveniet unde quae tempora voluptas facere porro! Natus, nisi
-        hic accusantium aut, ratione cupiditate perferendis provident, maiores a
-        adipisci beatae?
-      </p>
+      {!isEditing && (
+        <button
+          className="text-white bg-sky-600 hover:bg-sky-700 p-1 w-48 mt-2 rounded-md"
+          onClick={() => setIsEditing(true)}
+        >
+          Edit profile
+        </button>
+      )}
+
+      {isEditing && (
+        <div>
+          <button
+            className=" text-white bg-green-400 hover:bg-green-500 p-1 w-12 ml-7 mr-2 rounded-md"
+            onClick={updateProfile}
+          >
+            Save
+          </button>
+          <button
+            className="text-white bg-gray-500 hover:bg-gray-600 p-1 w-20 rounded-md"
+            onClick={() => setIsEditing(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      )}
+
       <p className="font-bold my-4">Services</p>
       <Pill services={services} />
       <p className="font-bold my-4">Portfolio</p>
