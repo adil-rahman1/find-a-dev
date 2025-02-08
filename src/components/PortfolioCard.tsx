454d254a2f0170f52a96c3fa7bb453246ead5c5d
Adil Rahman <82183381+adil-rahman1@users.noreply.github.com>
2025-02-08 12:05:24 +0000

    Added skills to PortfolioCard using pills

--- src/components/PortfolioCard.tsx
+++ src/components/PortfolioCard.tsx
@@ -1,3 +1,7 @@
+import Pill from "./Pill";
+
+const skills = ["Web development", "Mobile development"];
+
 const PortfolioCard = () => {
   return (
     <div className="border border-stone-300 rounded-xl p-4 flex gap-4">
@@ -6,14 +10,21 @@ const PortfolioCard = () => {
         src="/public/assets/portfolio-image.webp"
         alt=""
       />
-      <div>
-        <p className="text-xl font-medium mb-2">
-          Paple website - Frontend Design
-        </p>
+      <div className="flex flex-col justify-around gap-4">
+        <p className="text-xl font-medium">Paple website - Frontend Design</p>
         <p className="text-sm">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea magnam
-          nobis fuga consequatur, non doloribus.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. At, saepe
+          officia! Quis quaerat non et fugit ex? Odit tempora quae aspernatur ab
+          asperiores distinctio obcaecati nisi sapiente, atque consequatur
+          recusandae doloremque iure vero unde doloribus nulla accusamus
+          ratione. Consectetur veniam autem, atque asperiores vel molestias sint
+          explicabo id vitae debitis?
         </p>
+        <div className="flex flex-row gap-4">
+          {skills.map((skill) => (
+            <Pill text={skill} />
+          ))}
+        </div>
       </div>
     </div>
   );
